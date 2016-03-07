7ad476a1b97c33bc48112d17ae4d785f6d7afede
Jon Barlow <orviwan@gmail.com>
2016-03-07 20:52:04 +0000

    peer review fixes

--- src/Strider.c
+++ src/Strider.c
@@ -7,8 +7,8 @@ static TextLayer *s_time_layer, *s_step_layer;
 static char s_current_time_buffer[8], s_current_steps_buffer[16];
 static int s_step_count = 0, s_step_goal = 0, s_step_average = 0;
 
-// Is health data available?
-bool is_health_available() {
+// Is step data available?
+bool step_data_is_available() {
   return HealthServiceAccessibilityMaskAvailable &
     health_service_metric_accessible(HealthMetricStepCount,
       time_start_of_today(),
@@ -17,9 +17,11 @@ bool is_health_available() {
 
 // Daily step goal
 static void get_step_goal() {
+  const time_t start = time_start_of_today();
+  const time_t end = start + SECONDS_PER_DAY;
   s_step_goal = (int)health_service_sum_averaged(HealthMetricStepCount,
-    time_start_of_today(),
-    time_start_of_today() + SECONDS_PER_DAY,
+    start,
+    end,
     HealthServiceTimeScopeDaily);
 }
 
@@ -30,31 +32,33 @@ static void get_step_count() {
 
 // Average daily step count for this time of day
 static void get_step_average() {
+  const time_t start = time_start_of_today();
+  const time_t end = start + (time(NULL) - start);
   s_step_average = (int)health_service_sum_averaged(HealthMetricStepCount,
-    time_start_of_today(),
-    time_start_of_today() + (time(NULL) - time_start_of_today()),
+    start,
+    end,
     HealthServiceTimeScopeDaily);
 }
 
 static void display_step_count() {
   int thousands = s_step_count / 1000;
   int hundreds = s_step_count % 1000;
-  static char emoji[5];
+  static char s_emoji[5];
 
   if(s_step_count >= s_step_average) {
     text_layer_set_text_color(s_step_layer, GColorJaegerGreen);
-    snprintf(emoji, sizeof(emoji), "\U0001F60C");
+    snprintf(s_emoji, sizeof(s_emoji), "\U0001F60C");
   } else {
     text_layer_set_text_color(s_step_layer, GColorPictonBlue);
-    snprintf(emoji, sizeof(emoji), "\U0001F4A9");
+    snprintf(s_emoji, sizeof(s_emoji), "\U0001F4A9");
   }
 
   if(thousands > 0) {
     snprintf(s_current_steps_buffer, sizeof(s_current_steps_buffer),
-      "%d,%03d %s", thousands, hundreds, emoji);
+      "%d,%03d %s", thousands, hundreds, s_emoji);
   } else {
     snprintf(s_current_steps_buffer, sizeof(s_current_steps_buffer),
-      "%d %s", hundreds, emoji);
+      "%d %s", hundreds, s_emoji);
   }
 
   text_layer_set_text(s_step_layer, s_current_steps_buffer);
@@ -100,7 +104,7 @@ static void window_load(Window *window) {
   layer_add_child(s_window_layer, text_layer_get_layer(s_step_layer));
 
   // Subscribe to health events if we can
-  if(is_health_available()) {
+  if(step_data_is_available()) {
     health_service_events_subscribe(health_handler, NULL);
   }
 }
@@ -115,11 +119,10 @@ void init() {
   s_window_layer = window_get_root_layer(s_window);
   window_set_background_color(s_window, GColorBlack);
 
-  window_set_window_handlers(s_window,
-                             (WindowHandlers) {
-                                 .load = window_load,
-                                 .unload = window_unload
-                             });
+  window_set_window_handlers(s_window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload
+  });
 
   window_stack_push(s_window, true);
 
